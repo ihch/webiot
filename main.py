1d0acdfceb39ca0cd6005e83876f9ba51922ca67
nemusou <ryubi.102395@gmail.com>
2018-02-25 10:28:06 +0900

    dbからの取り方を変えます

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from sqlalchemy import Column, Integer, String, Float, DateTime
 from models import VigilancePosition, Sensor
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+engine = create_engine(os.environ["DATABASE_URL"], echo=True)
+db.Base.metadata.bind = engine
+db.Base.metadata.create_all(engine)
+session = sessionmaker(bind=engine)()
 
 
 @app.route('/')
@@ -43,13 +45,13 @@ def add_sensor():
 
 @app.route("/get/vigilance_position")
 def get_vigilance_position():
-    read = VigilancePosition.query.all()
+    read = session.query(db.VigilancePosition).all()
     return repr(read)
 
 
 @app.route("/get/sensor_position")
 def get_sensor_position():
-    read = Sensor.query.all()
+    read = session.query(db.Sensor).all()
     return repr(read)
 
 
