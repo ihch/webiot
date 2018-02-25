6438a40dab77442838ede96f0165deddeda4fd73
nemusou <ryubi.102395@gmail.com>
2018-02-25 13:40:22 +0900

    id=1

--- models.py
+++ models.py
@@ -7,13 +7,14 @@ Base = declarative_base()
 class VigilancePosition(Base):
     __tablename__ = "vigilanceposition"
 
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True)
     discover_time = Column(String(100))
     latitude = Column(Float, primary_key=True)
     longitude = Column(Float, primary_key=True)
     youtube_id = Column(String, primary_key=True)
 
     def __init__(self, discover_time, latitude, longitude, youtube_id):
+        self.id = 1
         self.discover_time = discover_time
         self.latitude = latitude
         self.longitude = longitude
@@ -27,11 +28,12 @@ class VigilancePosition(Base):
 class Sensor(Base):
     __tablename__ = "sensors"
 
-    id = Column(Integer)
+    id = Column(Integer, primary_key=True)
     latitude = Column(Float)
     longitude = Column(Float)
 
     def __init__(self, latitude, longitude):
+        self.id = 1
         self.latitude = latitude
         self.longitude = longitude
 
