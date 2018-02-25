bd52bd993243f11b074afb3b17e3deb1a8c0e4c7
nemusou <ryubi.102395@gmail.com>
2018-02-25 10:20:04 +0900

    db. を消した

--- models.py
+++ models.py
@@ -8,9 +8,9 @@ class VigilancePosition(Base):
     __tablename__ = "vigilanceposition"
 
     id = Column(Integer, primary_key=True)
-    discover_time = db.Column(db.String(100))
-    latitude = db.Column(db.Float, primary_key=True)
-    longitude = db.Column(db.Float, primary_key=True)
+    discover_time = Column(String(100))
+    latitude = Column(Float, primary_key=True)
+    longitude = Column(Float, primary_key=True)
 
     def __init__(self, discover_time, latitude, longitude):
         self.discover_time = discover_time
@@ -25,9 +25,9 @@ self.id, self.latitude, self.longitude)
 class Sensor(Base):
     __tablename__ = "sensors"
 
-    id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
+    id = Column(Integer, primary_key=True)
+    latitude = Column(Float)
+    longitude = Column(Float)
 
     def __init__(self, latitude, longitude):
         self.latitude = latitude
