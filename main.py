1d26f2f8a16cdcad64aebd255dc4b8fe32222f4e
nemusou <ryubi.102395@gmail.com>
2018-02-25 10:05:42 +0900

    Model定義を分離

--- main.py
+++ main.py
@@ -3,24 +3,16 @@ from collections import namedtuple
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Float, DateTime
+from models import VigilancePosition, Sensor
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, echo=True)
 
 
-class DiscoverInosisi(db.Model):
-    discover_time = db.Column(db.String(100))
-    position_x = db.Column(db.Float, primary_key=True)
-    position_y = db.Column(db.Float, primary_key=True)
-
-    def __init__(self, discover_time, position_x, position_y):
-        self.discover_time = discover_time
-        self.position_x = position_x
-        self.position_y = position_y
-
-    # def __repr__(self):
-    #     return "{}\n(x, y): ({}, {})".format(self.discover_time, self.position_x, self.position_y)
+@app.route('/')
+def index():
+    return "hoge"
 
 
 @app.route('/test')
@@ -28,18 +20,30 @@ def test():
     return "Hello, World!"
 
 
-@app.route('/post/data', methods=['POST'])
-def data():
-    data = DiscoverInosisi(
+@app.route('/add/vigilance_position', methods=['POST'])
+
+def add_vigilance_position():
+    data = VigilancePosition(
             request.form['discover_time'],
-            float(request.form['position_x']),
-            float(request.form['position_y'])
+            float(request.form['latitude']),
+            float(request.form['longitude'])
             )
-    db.session.add(
-            data
+    add_db(data)
+
+
+@app.route('/add/sensor')
+def add_sensor():
+    data = Sensor(
+            float(request.form['latitude'])
+            float(request.form['longitude'])
             )
+    add_db(data)
+
+
+def add_db(new_cls):
+    db.session.add(new_cls)
+    # db.session.flush()
     db.session.commit()
-    print(d)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,12 +1,43 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
+from flask_sqlalchemy import SQLAlchemy
 
 
-class Position:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class VigilancePosition(db.Model):
+    __tablename__ = "vigilanceposition"
+
+    id = Column(Integer, primary_key=True)
+    discover_time = db.Column(db.String(100))
+    latitude = db.Column(db.Float, primary_key=True)
+    longitude = db.Column(db.Float, primary_key=True)
+
+    def __init__(self, discover_time, latitude, longitude):
+        self.discover_time = discover_time
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return "Sensor(id={}, latitude={}, longitude={})".format(
+self.id, self.latitude, self.longitude)
 
 
-class DiscoverInosisi(db.Model):
-    discover_time = Column(Integer, primary_key=True)
-    position = Column(Position, primary_key=True)
+class Sensor(db.Model):
+    __tablename__ = "sensors"
+
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def __repr__(self):
+        return "Sensor(id={}, latitude={}, longitude={})".format(
+self.id, self.latitude, self.longitude)
+
+
+if __name__ == '__main__'
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    db = SQLAlchemy(app, echo=True)
+
